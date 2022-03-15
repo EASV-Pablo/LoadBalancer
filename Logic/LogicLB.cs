4a1152241a063251256ace941c2bd92e9c892f4d
EASV-Pablo <pabl0311@easv365.dk>
2022-03-15 11:39:23 +0100

    GetOptimalMachine implemented

--- Logic/LogicLB.cs
+++ Logic/LogicLB.cs
@@ -1,15 +1,65 @@
 ﻿using LoadBalancer.Models;
+using RestSharp;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace LoadBalancer.Logic
 {
     public class LogicLB
     {
-        public static Machine getMachine()
+        public Machine getMachine()
         {
             IOrderedEnumerable<Machine> machines = Program.machines.OrderByDescending(x => x.Property).ThenBy(x => decimal.Divide(x.ReqUsed, x.MaxReq) * 100);
 
             return machines.First();
+        }
+
+        public OutputDto requestSunset(InputDto input)
+        {
+            Machine machine = getMachine();
+            RestClient client = new RestClient(new Uri(machine.Url, "sun/sunset"));
+            var request = new RestRequest();
+            request.AddJsonBody(input);
+            var response = client.GetAsync<OutputDto>(request);
+            response.Wait();
+
+            return response.Result;
+        }
+
+        public OutputDto requestSunrise(InputDto input)
+        {
+            Machine machine = getMachine();
+            RestClient client = new RestClient(new Uri(machine.Url, "sun/sunrise"));
+            var request = new RestRequest();
+            request.AddJsonBody(input);
+            var response = client.GetAsync<OutputDto>(request);
+            response.Wait();
+
+            return response.Result;
+        }
+
+        public bool existsMachine(Machine machine)
+        {
+            if (Program.machines.Exists(x => x.Url == machine.Url))
+                return true;
+
+            return false;
+        }
+
+        public void addMachine(Machine machine)
+        {
+            Program.machines.Add(machine);
+        }
+
+        public void removeMachine(Machine machine)
+        {
+            Program.machines.Remove(machine);
+        }
+    
+        public List<Machine> getAllMachines()
+        {
+            return Program.machines;
         }
 
     }
